144115d78e845a6e033f1df653bdae34a43869d9
Kristen Carethers <kcarethers@gmail.com>
2016-09-13 17:12:41 -0400

    Checked math and works.  Added a do while loop

--- src/LabFive.java
+++ src/LabFive.java
@@ -1,42 +1,45 @@
 import java.util.Scanner;
 
-
 public class LabFive {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		String answer;
-		
-		//System.out.println("Welcome to the Grand Circus Casino! Roll the dice? (y/n): ");
-		//answer=
-		
+
+		// System.out.println("Welcome to the Grand Circus Casino! Roll the
+		// dice? (y/n): ");
+		// answer=
+
 		Scanner input = new Scanner(System.in);
-		
-		//do {
+
+		String cont;
+
+		do {
 			System.out.println("Enter the Number of sides for the dice:");
 			int capture = input.nextInt();
-			
+
 			double x = Math.random() * capture;
 			long result = (long) x;
-			
+
 			double y = Math.random() * capture;
 			long result2 = (long) y;
-			
-			System.out.println(result + result2);
-			
-			
-		//} while (condition);
-		
-		
-		
-		
-		
-	}//psvm
 
-}//class
+			System.out.println(result);
+			System.out.println(result2);
 
-//download scanner method
-//make a new scanner
-//sysout - ask user to enter number of sides of a pair of dice.
-//capture temporary user data and put into new data type + variable name (i.e. String name)
+			System.out.println("Continue y/n: ");
+			cont = input.nextLine();
+
+		} while (cont.equals("y"));
+		
+
+	}// psvm
+
+}// class
+
+// download scanner method
+// make a new scanner
+// sysout - ask user to enter number of sides of a pair of dice.
+// capture temporary user data and put into new data type + variable name (i.e.
+// String name)
